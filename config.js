aa438a4465d52dfadb5be43a4b38cecd748c8e9f
Austin <77835617+AustinBoath@users.noreply.github.com>
2022-06-30 22:55:59 +0930

    fixed a link glitch

--- config.js
+++
@@ -1,9 +0,0 @@
-export let config = {
-    name: 'John Doe',
-    imageURL: 'profileImage.png',
-    links: [
-        { title: 'Example #1', url: 'https://example.com/' },
-        { title: 'Example #2', url: 'https://example.com/' },
-        { title: 'Example #3', url: 'https://example.com/' },
-    ]
-}
